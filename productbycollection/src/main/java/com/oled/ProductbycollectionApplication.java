d791bb61daadb7fcf2a42d5a576e03fed9d145c6
Shree <Shree@DESKTOP-8BS0DM0>
2023-03-07 03:38:54 +0530

    commit

---
+++ productbycollection/src/main/java/com/oled/ProductbycollectionApplication.java
@@ -0,0 +1,13 @@
+package com.oled;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductbycollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductbycollectionApplication.class, args);
+	}
+
+}
---
+++ productbycollection/src/test/java/com/oled/ProductbycollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oled;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductbycollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
